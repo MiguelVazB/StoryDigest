df738a55a028b87c0e2045bdd4058ac37243c509
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-09-27 17:21:21 -0700

    updated format

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-removebg.png";
 
 const Hero = () => {
   return (
-    <div className="gap-4">
-      <header>
+    <div>
+      <header className="flex items-center gap-1 p-2 mb-10">
         <img className="w-20 h-20" src={logo} alt="logo" />
+        <div className="text-3xl font-extrabold">StoryDigest</div>
       </header>
-      <div className="flex flex-col gap-2 items-center text-center justify-center">
-        <div className="text-4xl font-bold">
+      <div className="flex flex-col gap-8 items-center text-center justify-center">
+        <div className="text-5xl font-bold">
           <p>Summarize Articles with</p>
-          <span className="text-blue-500 font-bold">OpenAi GPT-4</span>
+          <span className="bg-gradient-to-r from-amber-500 to-pink-600 text-transparent bg-clip-text font-extrabold">
+            OpenAI GPT-4
+          </span>
         </div>
-        <p className="text-lg text-gray-700">
+        <p className="text-xl text-gray-600 max-w-4xl">
           Simplify your reading with StoryDigest, an open-source article
           summarizer. StoryDigest uses the power of GPT-4 to summarize long
           articles into short and concise summaries.

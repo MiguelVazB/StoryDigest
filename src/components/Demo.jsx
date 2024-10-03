b01839bf109f61c80e1fc52344612c8813869a89
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-10-03 16:21:30 -0700

    Initial implementation of Demo component
    Added a form that contains an input and submit button. Added styling and added a placeholder function to handle when the user clicks on Summarize

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-[85%] bg-white items-center justify-center rounded-md overflow-hidden shadow-lg"
+    >
+      <input
+        className="w-full max-w-3xl p-2"
+        type="text"
+        placeholder="Enter article URL"
+      />
+      <button
+        type="submit"
+        className="bg-black hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-r transition duration-300 ease-in-out"
+      >
+        Summarize
+      </button>
+    </form>
+  );
 };
 
 export default Demo;

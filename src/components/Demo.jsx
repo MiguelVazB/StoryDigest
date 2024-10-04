a48c673c8b2b03c156ac916165e8ba9ed7944696
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-10-03 17:47:47 -0700

    Update Demo.jsx
    Fixed the form's input and added logic to handle fetching the data and updating the states

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  const handleSubmit = (e) => {
+  const [article, setArticle] = useState({ url: "", summary: "" });
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const [allArticles, setAllArticles] = useState([]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Form submitted");
+    const data = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+    }
   };
+
+  useEffect(() => {
+    if (article.summary) {
+      console.log(article.summary);
+    }
+  }, [article.summary]);
 
   return (
     <form
       onSubmit={handleSubmit}
+      name="article-summary-form"
       className="flex w-[85%] bg-white items-center justify-center rounded-md overflow-hidden shadow-lg"
     >
       <input
-        className="w-full max-w-3xl p-2"
-        type="text"
+        className="w-full p-2"
+        type="url"
         placeholder="Enter article URL"
+        value={article.url}
+        onChange={(e) => setArticle({ ...article, url: e.target.value })}
+        required
       />
       <button
         type="submit"

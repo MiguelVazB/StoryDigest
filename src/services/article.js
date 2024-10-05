1c04c8ff292dc89b7845a0ac383edd64626472b8
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2024-10-05 02:40:41 -0700

    Refactor article.js to use environment variables for RapidAPI key and host

--- src/services/article.js
+++ src/services/article.js
@@ -1,15 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidapiKey = import.meta.env.VITE_RAPIDAPI_KEY;
-const rapidapiHost = import.meta.env.VITE_RAPIDAPI_HOST;
-
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com",
     prepareHeaders: (headers) => {
-      headers.set("x-rapidapi-key", rapidapiKey);
-      headers.set("x-rapidapi-host", rapidapiHost);
+      headers.set("x-rapidapi-key", import.meta.env.VITE_RAPIDAPI_KEY);
+      headers.set("x-rapidapi-host", import.meta.env.VITE_RAPIDAPI_HOST);
 
       return headers;
     },
